561149eab4fc2b2dc42debe9aeaf6a5aa067948f
Riley-Davis <tul03435@temple.edu>
2026-04-02 12:05:27 -0400

    Connected timer to textView

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 import java.util.Timer
 import kotlin.concurrent.timer
 
@@ -14,9 +17,16 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var timerBinder: TimerService.TimerBinder
     var isConnected = false
+
+    lateinit var timerTextView: TextView
+    val timerHandler = Handler(Looper.getMainLooper()){
+        timerTextView.text = it.what.toString()
+        true
+    }
     val serviceConnection = object : ServiceConnection{
         override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
             timerBinder = p1 as TimerService.TimerBinder
+            timerBinder.setHandler(timerHandler)
             isConnected = true
         }
 
@@ -47,6 +57,8 @@ class MainActivity : AppCompatActivity() {
             serviceConnection,
             BIND_AUTO_CREATE
         )
+
+        timerTextView = findViewById<TextView>(R.id.textView)
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
             if (isConnected){
