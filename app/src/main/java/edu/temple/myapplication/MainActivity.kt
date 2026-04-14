757254f1dbf13344b0d53f107bc9eace8c2b5c79
Riley-Davis <tul03435@temple.edu>
2026-04-14 11:52:36 -0400

    Added menu functionality

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -10,6 +10,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import java.util.Timer
@@ -54,6 +56,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         bindService(
             Intent(this, TimerService::class.java),
             serviceConnection,
@@ -87,8 +91,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun startAction(){
+        if (isConnected){
+            if(timerBinder.paused || !timerBinder.isRunning){
+                timerBinder.start(10)
+            }
+            else{
+                timerBinder.pause()
+                            }
+
+        }
+    }
+
+    fun stopAction(){
+        if (isConnected) timerBinder.stop()
+    }
+
     override fun onDestroy() {
         unbindService(serviceConnection)
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.actionStart -> startAction()
+            R.id.actionStop -> stopAction()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
